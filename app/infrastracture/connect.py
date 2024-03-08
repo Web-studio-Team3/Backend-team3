c8cbfb5725bf4d8e4d171e6e552579588d17ad56
H1kkar1 <kn_pavel@mail.ru>
2024-03-08 13:08:20 +0300

    вернул стандартное подключения для пулл реквеста

--- app/infrastracture/connect.py
+++ app/infrastracture/connect.py
@@ -1,11 +1,9 @@
 from envparse import Env
 from pymongo import MongoClient
-from pymongo.database import Database
 
 env = Env()
-# MONGODB_URL = env.str("MONGODB_URL", default="mongodb://localhost:27017")
-MONGODB_URL = env.str("MONGODB_URL", default="mongodb://localhost:27017/")
+MONGODB_URL = env.str("MONGODB_URL", default="mongodb://localhost:27017")
 
 connection = MongoClient(MONGODB_URL)
-# database = connection["baraholka"]
-database = connection["Chats"]
+database = connection["baraholka"]
+
--- config/db.py
+++ config/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
 conn = MongoClient("mongodb://localhost:27017")
-# db = conn.baraholka
-db = conn.Chats
+db = conn.baraholka
+
