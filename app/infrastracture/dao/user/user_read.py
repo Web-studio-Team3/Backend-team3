4dca80634142d590829055cc2ecfc99ad97cdbd5
H1kkar1 <kn_pavel@mail.ru>
2024-05-07 00:26:39 +0300

    мелкие фиксы в User

--- app/infrastracture/dao/user/user_read.py
+++ app/infrastracture/dao/user/user_read.py
@@ -9,16 +9,17 @@ from app.infrastracture.dao.base import BaseDao
 class UserReadDaoImpl(BaseDao, UserRead):
     def get_by_email(self, email: str) -> UserGetByEmailResp:
         user = self._database["user"].find_one({"email": email})
-        if not user:
-            raise TypeError
-        return UserGetByEmailResp(
-            user_id=str(user["_id"]), hashed_password=str(user["hashed_password"])
-        )
+        if user is None:
+            raise Exception('User with this email was not found in the database')
+        else:
+            return UserGetByEmailResp(
+                user_id=str(user["_id"]), hashed_password=str(user["hashed_password"])
+            )
 
     def get_by_id(self, id: str) -> User:
         user = self._database["user"].find_one({"_id": ObjectId(id)})
         if not user:
-            raise TypeError
+            raise Exception('User with this id was not found in the database')
         return User(
             id=str(user["_id"]),
             email=user["email"],
@@ -26,6 +27,6 @@ class UserReadDaoImpl(BaseDao, UserRead):
             full_name=user["full_name"],
             date_of_birth=user["date_of_birth"],
             picture_id=user["picture_id"],
-            telegram_id=user.get("telegram_username"),
+            telegram_id=user.get("telegram_id"),
             telegram_username=user.get("telegram_username")
         )
